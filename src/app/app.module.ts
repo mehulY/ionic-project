03c1732b1f113f1ddf76e596e859c67f89f4f2a2
mehulY <mehul.yadav.k@gmail.com>
2019-08-19 16:49:19 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ParallaxHeader } from '../components/parallax-header/parallax-header';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage, 
+    ParallaxHeader
   ],
   imports: [
     BrowserModule,
@@ -22,9 +21,6 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    {provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
